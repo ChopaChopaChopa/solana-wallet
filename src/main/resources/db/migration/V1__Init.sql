2606c1302fbad2aa999801b9f816f15fc68576dd
ChopaChopaChopa <ilyach.acc@gmail.com>
2023-08-20 14:53:51 +0300

    Removed transactions table

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -1,13 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS transactions(
-    id           UUID NOT NULL DEFAULT uuid_generate_v4(),
-    from_address VARCHAR(44) NOT NULL,
-    to_address   VARCHAR(44) NOT NULL,
-    amount       BIGINT NOT NULL,
-    date_time    TIMESTAMP NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS users(
     id         UUID NOT NULL DEFAULT uuid_generate_v4(),
     username   VARCHAR(32) NOT NULL,
